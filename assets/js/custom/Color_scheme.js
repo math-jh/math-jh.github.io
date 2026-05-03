192a17aea996f4b26fb94c41a1fec650fce2ea16
Junhyeok Kim <kujuburi@icloud.com>
2026-05-03 15:58:44 +0900

    refactor: scrollbar JS→CSS, clean up tracked junk files
    - Move scrollbar thumb colors from dynamic JS injection to static CSS
    - Remove empty <style id="scrollbar-color"> and onload call
    - Update .gitignore for .backups, .obsidian, .bundle, *.backup_*
    - Untrack 14 cached files (backups, obsidian config, bundle config)

--- assets/js/custom/Color_scheme.js
+++ assets/js/custom/Color_scheme.js
@@ -43,22 +43,5 @@ function toggleDarkTheme() {
             toggleThemeBtn.checked = defaultTheme.disabled
         }
         
-        // 스크롤바 업데이트
-        scrollbar();
     }
 }
-
-function scrollbar() {
-if(document.getElementById("toggle_dark_theme").checked == true)
-document.head.querySelector("#scrollbar-color").innerHTML=`
-   body::-webkit-scrollbar-thumb {background-color:#455a64; border-radius: 3px; background-clip: padding-box; border:2px solid transparent}
-   .sidebar.sticky::-webkit-scrollbar-thumb {background-color:#282828; border-radius: 3px; background-clip: padding-box; border:1px solid transparent}
-   .toc__menu::-webkit-scrollbar-thumb {background-color:#282828; border-radius: 3px; background-clip: padding-box; border:1px solid transparent}
- `
- else
- document.head.querySelector("#scrollbar-color").innerHTML=`
-   body::-webkit-scrollbar-thumb {background-color:#cfd8dc; border-radius: 3px; background-clip: padding-box; border:2px solid transparent}
-   .sidebar.sticky::-webkit-scrollbar-thumb {background-color:#eaeaf2; border-radius: 3px; background-clip: padding-box; border:1px solid transparent}
-   .toc__menu::-webkit-scrollbar-thumb {background-color:#eaeaf2; border-radius: 3px; background-clip: padding-box; border:1px solid transparent}
- `
- }
